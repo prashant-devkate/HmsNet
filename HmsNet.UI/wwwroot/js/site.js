84419e51b11f9e7d831753244f898faeacd0c91d
prashant devkate <prashantdevkate1999@gmail.com>
2025-08-05 21:56:51 +0530

    Add Reports/Transactions menu in navbar

--- HmsNet.UI/wwwroot/js/site.js
+++ HmsNet.UI/wwwroot/js/site.js
@@ -1,4 +1,21 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿$(document).ready(function () {
+    // Navbar scroll effect
+    $(window).scroll(function () {
+        if ($(window).scrollTop() > 50) {
+            $('.navbar').addClass('scrolled');
+        } else {
+            $('.navbar').removeClass('scrolled');
+        }
+    });
 
-// Write your JavaScript code.
+    // Highlight active nav link based on exact URL match
+    const currentPath = window.location.pathname;
+    $('.nav-link').each(function () {
+        const linkPath = $(this).attr('href');
+        if (linkPath && currentPath === linkPath) {
+            $(this).addClass('active');
+        } else {
+            $(this).removeClass('active');
+        }
+    });
+});
